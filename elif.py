a3e997d242eb2f0a053c4b81ac7d8445b31c1cd1
Fadlin Nur Asyifa <fadlinnurasyifa@gmail.com>
2023-11-10 18:00:57 +0700

    ya

---
+++ elif.py
@@ -0,0 +1,8 @@
+nilai = float(input("Masukkan nilai Anda: "))
+
+if nilai >= 90:
+    print("Bagus")
+elif nilai >= 70:
+    print("Cukup")
+else:
+    print("Kurang")
---
+++ if else.py
@@ -0,0 +1,7 @@
+nilai = float(input("Masukkan Nilai Ujian :"))
+
+if nilai >= 75 :
+    print ('Lulus Ujian')
+
+else :
+    print ('Tidak Lulus Ujian')
